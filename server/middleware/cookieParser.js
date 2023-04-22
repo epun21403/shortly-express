a230fd5579f3a80a5269472e6e5d9bc22767ec9f
John Novakowski <jnovakowski797@gmail.com>
2023-04-22 15:37:54 -0400

    initial work in progress submission

--- server/middleware/cookieParser.js
+++ server/middleware/cookieParser.js
@@ -1,4 +1,17 @@
+let auth = require('./auth');
+
 const parseCookies = (req, res, next) => {
+
+  if (req.headers.cookie) {
+    let cookieSet = req.headers.cookie.split('; ');
+    cookieSet.forEach(function (element) {
+      let cookiePair = element.split('=');
+      req.cookies[cookiePair[0]] = cookiePair[1];
+    });
+  } else {
+    auth.createSession(req, res, next);
+  }
+  next();
 };
 
 module.exports = parseCookies;
